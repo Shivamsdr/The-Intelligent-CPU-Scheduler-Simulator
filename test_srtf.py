c6d511d9316e9145993bce76b3ade4070c2bf5bd
OM SINGH CHAUHAN <152875944+OmSingh2005@users.noreply.github.com>
2025-03-14 04:01:10 +0530

    Update test_srtf.py
    minor

--- test_srtf.py
+++ test_srtf.py
@@ -1,30 +1,20 @@
 import pandas as pd
 from algorithms import srtf
 
-# Path to the Excel file
 file_path = r'C:\Users\oms1n\OneDrive\Desktop\CS\test.xlsx'
 
-# Read process data from the Excel file
 print("Reading Excel file...")
 df = pd.read_excel(file_path, usecols=['id', 'a', 'b', 'p'])
 print("Excel file read successfully.\n\n")
 
-# Rename columns
 df.rename(columns={'a': 'arrival_time', 'b': 'burst_time', 'p': 'priority'}, inplace=True)
 print("Columns renamed.\n\n")
 
-
-# Convert DataFrame to list of dictionaries
 processes = df.to_dict('records')
-#print("Converted to list of dictionaries:", processes)
 
-
-# Run the SRTF scheduling algorithm
-#print("Running SRTF scheduling algorithm...")
 result = srtf(processes)
 print("SRTF scheduling algorithm completed.\n\n")
 
-# Print the results
 total_completion_time = 0
 total_turnaround_time = 0
 total_waiting_time = 0
@@ -35,7 +25,6 @@ for process in result:
     total_turnaround_time += process['turnaround_time']
     total_waiting_time += process['waiting_time']
 
-# Calculate and print averages
 n = len(result)
 avg_completion_time = total_completion_time / n
 avg_turnaround_time = total_turnaround_time / n
